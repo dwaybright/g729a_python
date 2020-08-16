18550dffbd2547595100dfadab639941fd5a24e3
Daniel Waybright <dwaybright@kcp.com>
2020-08-15 22:27:23 -0600

    basic testing started!

--- test/c/conformance/tests/basic_ops_test.c
+++ test/c/conformance/tests/basic_ops_test.c
@@ -8,4 +8,11 @@ int main(int argc, char *argv[] )
 {
     printf("I compiled!");
     printf("\n");
+
+    Word16 var1 = 15;
+    Word16 var2 = 10;
+
+    Word16 result = add(var1, var2);
+
+    printf("%d + %d = %d", var1, var2, result);    
 }
