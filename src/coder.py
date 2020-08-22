5ea706e4ad2c10a3c495acae5a95350e80e21625
Daniel Waybright <dwaybright@kcp.com>
2020-08-22 12:10:21 -0600

    moved files around

--- src/coder.py
+++ src/coder.py
@@ -1,3 +1,4 @@
+from bits import *
 from basic_op import *
 from ld8a import *
 from cod_ld8a import Init_Coder_ld8a, Coder_ld8a
@@ -13,34 +14,6 @@ serial = [0] * SERIAL_SIZE
 def initialize() -> None:
     Init_Pre_Process()
     Init_Coder_ld8a()
-
-
-def convertWord16ToIntegerList(word16data: bytearray) -> List[int]:
-    """
-    (i) word16data: bytearray - Word16 binary data as Python bytearray
-    (o) intListData: List[int] - list of Word16 values as Python integers
-    """
-    intListData = []
-    
-    for i in range(0, len(word16data), 2):
-        word16ByteChunk = word16data[i:i+1]
-        word16AsInt = int.from_bytes(word16ByteChunk, "little", signed=True)
-        intListData.append(word16AsInt)
-
-    return intListData
-
-def convertIntegerListToWord16(intListData: List[int]) -> bytearray:
-    """
-    (i) intListData: List[int] - list of Word16 values as Python integers
-    (o) word16data: bytearray - Word16 binary data as Python bytearray
-    """
-    word16data = bytearray()
-
-    for i in range(0, len(intListData)):
-        bytesValue = intListData[i].to_bytes(2, byteorder="little", signed=True)
-        word16data.extend(bytesValue)
-
-    return word16data
 
 
 def convertPCMToG729a(pcm16data: bytearray) -> Tuple[bytearray, int]:
@@ -62,3 +35,5 @@ def convertPCMToG729a(pcm16data: bytearray) -> Tuple[bytearray, int]:
         # prm2bits_ld8k( prm, serial) -> sends prm to serial
 
         outputG729a.extend(serial)
+
+    return (outputG729a, outputG729aFrameCount)
